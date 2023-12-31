879e6aaa2a95db779be9df6f6a3fcad53384c873
AmritHimanshu <amrit.ewp@gmail.com>
2023-12-31 19:22:37 +0530

    added active in footer

--- client/src/Pages/Footer/Footer.js
+++ client/src/Pages/Footer/Footer.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import HomeIcon from '@mui/icons-material/Home';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import AddIcon from '@mui/icons-material/Add';
 import SlideshowOutlinedIcon from '@mui/icons-material/SlideshowOutlined';
+import { NavLink, useLocation } from 'react-router-dom';
 
 function Footer() {
+
+    const { pathname } = useLocation();
+
     return (
-        <div className='py-2 grid grid-rows-1 grid-cols-5 place-items-center border-t-[2px] border-neutral-700'>
-            <div><HomeOutlinedIcon style={{ fontSize: "40px" }} /></div>
+        <div className='py-2 grid grid-rows-1 grid-cols-5 place-items-center border-t-[1px] border-neutral-700'>
+            <div><NavLink to="/">{pathname === "/" ? <HomeIcon style={{ fontSize: "40px" }} /> : <HomeOutlinedIcon style={{ fontSize: "40px" }} /> }</NavLink></div>
             <div><SearchOutlinedIcon style={{ fontSize: "40px" }} /></div>
             <div className='border-2 rounded-lg max-w-min'><AddIcon style={{ fontSize: "30px" }} /></div>
             <div><SlideshowOutlinedIcon style={{ fontSize: "40px" }} /></div>
-            <div className='w-[40px] h-[40px] rounded-full border-2 overflow-hidden'><img src="https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/61NRxcV4VML.jpg" alt="" className='w-[40px] h-[40px]' /></div>
+            <NavLink to="/profile"><div className={`w-[40px] h-[40px] rounded-full overflow-hidden ${pathname === "/profile"?'border-2':''}`}><img src="https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/61NRxcV4VML.jpg" alt="" className='w-[40px] h-[40px]' /></div></NavLink>
         </div>
     )
 }
