5b675f371c1010b1caf5d5a2a826c584767212cb
AmritHimanshu <amrit.ewp@gmail.com>
2024-01-03 16:51:03 +0530

    removed userImage

--- server/model/postSchema.js
+++ server/model/postSchema.js
@@ -13,10 +13,6 @@ const postSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    userImage: {
-        data: Buffer,
-        contentType: String
-    }
 });
 
 const Post = mongoose.model('POST', postSchema);
