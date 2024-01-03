b54a22f1e3f5f9d10fba52a6eae6d87f7099dd7d
AmritHimanshu <amrit.ewp@gmail.com>
2024-01-03 18:17:49 +0530

    removed postImage

--- server/router/auth.js
+++ server/router/auth.js
@@ -205,8 +205,8 @@ router.get('/getUserPost', authenticate, async (req, res) => {
 router.get('/getAllPost', async (req, res) => {
     // res.status(200).json({message:"hii this is get all post"})
     // try {
-    // const allPost = await Post.find();
-        res.status(200).send("jhgg");
+    const allPost = await Post.find();
+        res.status(200).send(allPost);
     // } catch (error) {
     //     console.log("GetAllPost" + error);
     // }
